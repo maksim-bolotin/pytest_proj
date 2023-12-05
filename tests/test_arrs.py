b99b4b374e289509a8f0b6266e8de27264004966
maksim-bolotin <150610016+maksim-bolotin@users.noreply.github.com>
2023-12-06 00:25:34 +0300

    updated test_slice.py (#2)

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -4,8 +4,13 @@ from utils import arrs
 def test_get():
     assert arrs.get([1, 2, 3], 1, "test") == 2
     assert arrs.get([], 0, "test") == "test"
+    assert arrs.get([1, 2, 3], -1, False) is False
+    assert arrs.get([1, 2, 3], 50, "test") == "test"
 
 
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([], -3, -1) == []
+    assert arrs.my_slice([1], -3) == [1]
+    assert arrs.my_slice([1, 2, 3, 4], None, 3) == [1, 2, 3]
